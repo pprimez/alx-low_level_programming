da5c24cbc9c483d7522cbb6ac179d50c17fdab1a
pprimez <abrahamoba78@gmail.com>
2022-09-09 13:06:24 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * This prints the program
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char ch
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
