de74ba8cb6646e7a253548227f869e11b477e472
pprimez <abrahamoba78@gmail.com>
2022-09-09 13:09:02 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - print if the number is positive, zero, or negative
- *
- * Description: using the main function
- * This prints the program
- * Return: 0
- */
-/* betty style doc for function main goes there */
+*main - main block
+*Description: Get a random number and check its last digit, compare it with 5
+*Return: 0
+*/
 int main(void)
 {
-	char ch
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+char c = 'a';
+
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+
+putchar('\n');
+return (0);
 }
